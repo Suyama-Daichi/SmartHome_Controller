7be2e1a5b98eb3a977b329be919db77d9fda2da6
SuyamaDaichi <symdit@gmail.com>
2020-08-23 16:41:48 +0900

    環境構築
    typescriptを導入

--- Aircon-wind-direction/index.ts
+++ Aircon-wind-direction/index.ts
@@ -1,8 +1,9 @@
-exports.handler = async (event) => {
+export function handler(event: any) {
+    console.log();
     // TODO implement
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
-};
+}

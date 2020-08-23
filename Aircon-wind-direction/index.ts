4d2a50209f8414183e7cc758fc53a89aa9885d7a
SuyamaDaichi <symdit@gmail.com>
2020-08-23 20:42:40 +0900

    環境構築
    TypeScriptのLambda開発環境を作成

--- Aircon-wind-direction/index.ts
+++
@@ -1,9 +0,0 @@
-export function handler(event: any) {
-    console.log();
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-}
--- Aircon-wind-direction/src/index.ts
+++ Aircon-wind-direction/src/index.ts
@@ -1,9 +1,5 @@
-export function handler(event: any) {
-    console.log('OK');
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+function hello(name: string): string {
+    return `Hello, ${name}!`;
 }
+
+console.log(hello("TypeScript"));

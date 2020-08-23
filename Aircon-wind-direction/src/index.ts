ec0c3c6e1ff27edcdff39a3aaae95f4768dd8d87
SuyamaDaichi <symdit@gmail.com>
2020-08-24 00:07:17 +0900

    パッケージ追加
    dotenvを追加

--- Aircon-wind-direction/src/index.ts
+++ Aircon-wind-direction/src/index.ts
@@ -1,15 +1,19 @@
 import { Context, APIGatewayEvent } from "aws-lambda";
 import axios from 'axios';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export async function handler(event: APIGatewayEvent, context?: Context): Promise<string> {
-    const token = process.env['remoToken'];
+    const token = process.env['REMO_TOKEN'];
     const baseRequest = axios.create({
-        baseURL: `api.nature.global`,
+        baseURL: `https://api.nature.global`,
         headers: { Authorization: `Bearer ${token}` },
         responseType: 'json'
     });
 
-    const res = await baseRequest.get('/1/devices');
-    console.log(res);
-    return `Hello, ${token}!`;
+    const res = await baseRequest.get('/1/devices')
+        .catch(e => {
+            console.error(e);
+        });
+    return res ? 'success!' : 'failured';
 }
